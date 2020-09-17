ff22b0c6f8710ab446918afc1d2652ee6aece2fb
Luu Van <vanngocluu94@gmail.com>
2020-09-17 22:10:03 +0700

    create home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/style.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar title="Oscars 2021" name="Actor"/>
+      <Navbar title="Oscars 2021" />
+      <Home title="Oscars Winner" />
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Navbar = (props) => (
     <nav>
-        <h2 class="logo"><a class="logo-link" href="#">{props.name}</a></h2>
+        <h2 class="logo"><a class="logo-link" href="#">{props.title}</a></h2>
         <ul class="nav-menu">
-          <li><a class="nav-menu__link" href="#">{props.name}</a></li>
-          <li><a class="nav-menu__link" href="#">Best Actors</a></li>
-          <li><a class="nav-menu__link" href="#">Best Actress</a></li>
-          <li><a class="nav-menu__link" href="#">Best Films</a></li>
+          <li><a className="nav-menu__link" href="#">Home</a></li>
+          <li><a className="nav-menu__link" href="#">Best Actors</a></li>
+          <li><a className="nav-menu__link" href="#">Best Actress</a></li>
+          <li><a className="nav-menu__link" href="#">Best Films</a></li>
         </ul>
       </nav>
 );
